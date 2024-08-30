2897bf67a18f42a484f18339fd8b9fbcd41d4758
Jon Green <jongreen1996@gmail.com>
2024-08-30 14:36:14 +0100

    Nuked supabase auth

--- src/app/actions.tsx
+++ src/app/actions.tsx
@@ -1,28 +1,15 @@
 'use server';
 
-import { createClient } from '@/lib/supabase/server';
 import { redirect } from 'next/navigation';
 import { z } from 'zod';
 
 export async function signUpForm(formData: FormData) {
   'use server';
 
-  const supabase = createClient();
   const email = formData.get('email') as string;
 
   if (z.string().email().safeParse(email).success === false)
     redirect('/?error=invalid_email');
 
-  const { error } = await supabase.auth.signInWithOtp({
-    email,
-    options: {
-      shouldCreateUser: true,
-      emailRedirectTo: `${process.env.BASE_URL}plans?email=${encodeURIComponent(email)}`,
-    },
-  });
-
-  if (error) redirect('/?error=error_signing_up');
-
-  // revalidatePath('/', 'layout');
-  redirect('/?success=true');
+  return redirect('/?success=true');
 }
