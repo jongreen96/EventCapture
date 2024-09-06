8dd98f4a644d6a14509e39f67bb7b09f0bae703e
Jon Green <jongreen1996@gmail.com>
2024-09-06 15:35:18 +0100

    Small UX improvements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
     <html lang='en' className='scroll-smooth'>
       <body
         className={cn(
-          'dark min-h-screen bg-background font-sans antialiased',
+          'dark mb-6 min-h-screen min-w-[430px] bg-background font-sans antialiased',
           inter.className,
         )}
       >
--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -6,7 +6,7 @@ export default async function Nav() {
   const user = session?.user;
 
   return (
-    <nav className='sticky top-0 z-50 bg-background/70 p-2 backdrop-blur'>
+    <nav className='sticky top-0 z-50 bg-background/70 py-2 backdrop-blur'>
       <div className='container flex justify-between'>
         <a href='/'>
           <p className='text-3xl font-semibold'>Event Capture</p>
