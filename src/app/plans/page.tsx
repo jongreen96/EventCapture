440f8173dfb0a61db0e43a84afb4874771a606e9
Jon Green <jongreen1996@gmail.com>
2024-09-09 21:46:14 +0100

    Fix plan page issue

--- src/app/plans/page.tsx
+++ src/app/plans/page.tsx
@@ -1,3 +1,4 @@
+import { getUserPlans } from '@/db/queries';
 import getSession from '@/lib/getSession';
 import { redirect } from 'next/navigation';
 import Plans from './_components/plans';
@@ -5,6 +6,8 @@ import Plans from './_components/plans';
 export default async function PlansPage() {
   const session = await getSession();
   if (!session?.user?.id) redirect('/');
+  const plan = await getUserPlans(session.user.id);
+  if (plan) redirect('/dashboard');
 
   return (
     <main>
