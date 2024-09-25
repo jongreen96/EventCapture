88c1baabc97072ff2a1cdd75aabdf3bc3f950a4a
Jon Green <jongreen1996@gmail.com>
2024-09-25 16:40:23 +0100

    Updated getPlan queries to not include expired plans

--- src/db/queries.ts
+++ src/db/queries.ts
@@ -1,5 +1,5 @@
 import { plansData } from '@/app/plans/_components/plans';
-import { and, eq } from 'drizzle-orm';
+import { and, eq, gt } from 'drizzle-orm';
 import { db } from './db';
 import { plans } from './schema';
 
@@ -13,7 +13,11 @@ export async function getUserPlan(userId: string, eventName: string) {
 
 export async function getUserPlans(userId: string) {
   const userPlans = await db.query.plans.findMany({
-    where: eq(plans.user, userId),
+    where: and(
+      eq(plans.user, userId),
+      // plan is not expired
+      gt(plans.endDate, new Date()),
+    ),
   });
 
   return userPlans;
