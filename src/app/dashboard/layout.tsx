c7ccd2e8fff8bd94701719152d2424c31d43813e
Jon Green <jongreen1996@gmail.com>
2024-10-14 20:05:29 +0100

    Refactor DashboardPage layout to use consistent padding

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -4,7 +4,7 @@ export default async function DashboardPage({
   children: React.ReactNode;
 }) {
   return (
-    <main className='container min-h-[calc(100vh-52px)] space-y-4 pt-6'>
+    <main className='container min-h-[calc(100vh-52px)] space-y-4 py-6'>
       <div className='flex items-center justify-between'>
         <h1 className='text-2xl font-bold tracking-tight md:text-4xl'>
           Dashboard
